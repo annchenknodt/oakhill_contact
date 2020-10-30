fc9a62c93e2c1930564e170be3dab97cf4eee803
annchenruth <aknodt@gmail.com>
2020-10-30 08:37:21 -0700

    updated settings for live site

--- contact/settings.py
+++ contact/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True # in settings_secret
 
-ALLOWED_HOSTS = ['ohhscontact.dreamhosters.com','127.0.0.1']
+ALLOWED_HOSTS = ['contact.oakhillhomesolutions.com', 'www.contact.oakhillhomesolutions.com', 'ohhscontact.dreamhosters.com','127.0.0.1']
 
 
 # Application definition
